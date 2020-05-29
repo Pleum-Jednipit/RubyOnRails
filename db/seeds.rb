67a8e7b88a20b6ef8ddebd5526a199343ee387e1
Jednipit <jednitan10@gmail.com>
2020-05-29 18:28:19 +0700

    Implemented custom scope for portfolio item

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
-		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		topic_id: Topic.last.id
 	)
 end
 
@@ -20,10 +27,20 @@ end
 	)
 end
 
-9.times do |portfolio|
+8.times do |portfolio|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio}",
-		subtitle: "My portflio",
+		subtitle: "Ruby on Rails",
+		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look eve",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200",
+	)
+end
+
+1.times do |portfolio|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio}",
+		subtitle: "Angular",
 		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look eve",
 		main_image: "http://placehold.it/600x400",
 		thumb_image: "http://placehold.it/350x200",
